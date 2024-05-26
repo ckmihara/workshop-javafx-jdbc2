1b4cd1029895ec7a5f3100dc683551bd98432a09
Celso Mihara <ckmihara@gmail.com>
2024-05-26 16:19:35 -0300

    Department ComboBox

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -117,6 +117,8 @@ public class SellerListController implements Initializable, DataChangeListener {
 			controller.setSeller(obj);
 
 			controller.setServices(new SellerService(), new DepartmentService());
+			
+			controller.loadAssociatedObjects();
 
 			controller.subscribeDataChangeListener(this);
 
@@ -130,6 +132,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
